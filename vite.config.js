074c190de17488ba3f7e82ebe04bea2fa715d3da
Bhakti Gupta <guptabhakti09@gmail.com>
2026-05-24 17:35:17 +0530

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Millineum-Book-Store/'
+  base: '/Millineum-Book-Store/',
+  build: {
+    outDir: 'docs'
+  }
 })
